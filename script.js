6b87f879855cabcba5f1d412e7faf8c3038582a6
Alex <dovgaloleksandr388@gmail.com>
2021-02-16 15:24:26 +0200

    finish project

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-var owl = $(".owl-carousel");
+let burger = document.querySelector('.header_burger');
+let burgerMenu = document.querySelector('.container_burger_menu');
+let closed = document.querySelector('.closed');
+
+burger.addEventListener('click', function(value){
+  burgerMenu.style.left = '0';
+  burgerMenu.style.transition = 'all 0.3s ease';
+})
+
+closed.addEventListener('click', function(value){
+  burgerMenu.style.left = '-300px';
+  burgerMenu.style.transition = 'all 0.3s ease';
+})
+
+
+
+var owl = $(".theatre_img .owl-carousel");
 $(function() {
     // Owl Carousel
     owl.owlCarousel({
@@ -14,4 +30,28 @@ $(function() {
    
   $(".prev_button").click(function(){
     owl.trigger("prev.owl.carousel");
+  });
+
+  $(function() {
+    // Owl Carousel
+    var owl = $(".stagings .owl-carousel");
+    owl.owlCarousel({
+      items: 4,
+        freeDrag: false,
+        autoWidth: true,
+        autoHeight: true,
+        loop: true
+    });
+  });
+
+$(function() {
+    // Owl Carousel
+    var owl = $(".partners .owl-carousel");
+    owl.owlCarousel({
+      items: 4,
+      loop: true,
+      nav: true,
+      autoWidth: true,
+      autoHeight: true
+    });
   });
